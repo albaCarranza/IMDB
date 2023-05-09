34614f50f63df9d79a1d6ae3a7b292511400ef13
albaCarranza <alba.carranza.g@gmail.com>
2023-05-10 01:09:17 +0200

    cambio

--- testimdb.ts
+++ testimdb.ts
@@ -2,7 +2,7 @@ import { Imdb } from "./imdb";
 import { Movie } from "./movie";
 
 let pepitoGrillo : Movie = new Movie ("Pepito Grillo", 2005, "Estadounidense", "familiar");
-let laSilla : Movie = new Movie ("La Silla", 1990, "Espanish", "terror");
+let laSilla : Movie = new Movie ("La Silla", 1990, "Spanish", "terror");
 
 
 let object : Imdb = new Imdb([pepitoGrillo, laSilla])
